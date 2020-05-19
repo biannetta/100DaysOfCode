d9a3771036c5972e806d658c3eca9317026d36cb
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2020-05-18 22:50:53 -0400

    Day 4: Fleshing out models
    Adding some detail to the model for Activities

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import { Activity } from "./models/index.js";
 import { testActivities } from "./_helpers/_testData.js";
 
-var activities = testActivities.reduce(function (accum, data) {
+const activities = testActivities.reduce((accum, data) => {
   accum.push(new Activity({type: data.type, quantity: data.quantity}));
   return accum;
 },[]);
 
-console.log(activities);
+const appContent = document.getElementById("content");
+const activityList = document.createElement('ul');
+activityList.setAttribute("id", "activity-list");
+
+activities.forEach( activity => {
+  let item = document.createElement('li');
+  item.innerHTML = activity.get('type');
+  activityList.appendChild(item);
+});
+
+appContent.appendChild(activityList);
--- src/models/Activity.js
+++ src/models/Activity.js
@@ -1,7 +1,17 @@
+const activityTypes = {
+  SITUPS: "Situps",
+  PUSHUPS: "Pushups",
+  SQUATS: "Squats",
+  PLANKS: "Planks"
+}
+
 export var Activity = Backbone.Model.extend({
   defaults: {
     type: '',
     quantity: 0,
     datetime: Date.now(),
+  },
+  validate: function (attrs) {
+    console.log(attrs);
   }
 });
