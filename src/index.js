fa8d7184eb716e3238667b00792f3fac016bffa6
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2020-06-29 21:56:55 -0400

    Day 32: App Panel Changes
    Changed the layout of the app content panels

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { AppRouter } from "./routers/router.js";
 import { AppView } from "./views/index.js";
 
 // Instantiate Router
-let router = new AppRouter();
+new AppRouter();
 Backbone.history.start();
 
 // Instantiate App Views
