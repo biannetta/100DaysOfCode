c2e6c769a91ad26b78cecb92e69ec9f3ce48e092
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2020-08-05 23:15:20 -0400

    Day 52: Creating Calendar Banner
    Idea for a calendar card banner that focus on todays date
    with yesterday and tomorrows date surrounding it
    Not sure I love the look. Will take it back to Figma and play around

--- src/views/CalendarBannerView.js
+++ src/views/CalendarBannerView.js
@@ -4,18 +4,21 @@ const alpha_dayOfWeek = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","F
 
 export const CalendarBannerView = Backbone.View.extend({
   tagName: 'div',
+  className: 'hdc_collection',
   initialize: function () {
     this.todaysDate = new Date();
   },
   render: function () {
-    let todayCard = new CardComponent({
-      className: "hdc_card hdc_card--calendar",
-      text: this.todaysDate.getDay(),
-      subtext: alpha_dayOfWeek[this.todaysDate.getDate()], 
-    });
+    for (var i of [-1, 0, 1]) {
+      // TODO: Refactor CSS of this, not sure i like nested ternary operators
+      let dateCard = new CardComponent({
+        className: "hdc_card hdc_card--calendar" + (i < 0 ? ' scaled scaled--left': i > 0 ? ' scaled scaled--right': ''),
+        text: this.todaysDate.getDate() + i,
+        subtext: alpha_dayOfWeek[this.todaysDate.getDay() + i], 
+      });
+      this.$el.append(dateCard.render().el);
+    }
 
-    this.$el.append(todayCard.render().el);
-    
     return this;
   }
 });
